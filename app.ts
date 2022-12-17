c248fd3fb080059e527196750bc0f506487fffd5
Arsanandha Abhisidvorachodi <arsanandha.ap@gmail.com>
2022-12-18 01:51:37 +0700

    :rocket: implementation: Full Implementation for Simple Pub/Sub Events
    - Add subscribe / publish / unsubscribe
    - Add LowStockWarningEvents / StockLevelOkEvents
    - Implements MachineRefillEvents
    - Add StockWarningSubscriber / MachineRefillSubscriber into Code
    - Add StockLevelOkEvent / LowStockWarningEvent into Code

--- app.ts
+++ app.ts
@@ -1,3 +1,14 @@
+/**
+ * Simple Pub-Sub Design Pattern
+ *
+ * Code by ARSANANDHA.XYZ
+ *
+ * references tutorial and guideline from : https://www.youtube.com/watch?v=f3Cn0CGytSQ
+ * references tutorial and guideline from : https://dev.to/jucian0/pub-sub-pattern-a-brief-explanation-21ed
+ *
+ * :bow-down: :bow-down: :bow-down:
+ */
+
 // interfaces
 interface IEvent {
   type(): string;
@@ -9,9 +20,78 @@ interface ISubscriber {
 }
 
 interface IPublishSubscribeService {
-  publish (event: IEvent): void;
+  // change signature to array for support firing low stock & stock status events
+  publish (event: IEvent[]): void;
   subscribe (type: string, handler: ISubscriber): void;
-  // unsubscribe ( /* Question 2 - build this feature */ );
+  // unsubscribe by use type for unsubscribe whole type
+  unsubscribe (type: string): void;
+}
+
+class PubSubService implements IPublishSubscribeService {
+  // add subscribers as Array<IEvent>
+  private subscribers: Array<IEvent>;
+
+  constructor() {
+    // initialize subscribers as new Array<IEvent>
+    this.subscribers = new Array<IEvent>();
+  }
+
+  publish(event: IEvent[]) {
+    // iterate event from arg(event) for push events into subscribers array
+    event.forEach((evt: IEvent) => {
+      this.subscribers.push(evt);
+    })
+  }
+
+  subscribe(type: string, handler: ISubscriber): void {
+    // iterate each subscriber and comparison for same type event to correct true function for proper function calling
+    this.subscribers.forEach((event: IEvent) => {
+      if (event.type() == type) {
+        handler.handle(event);
+      }
+    })
+  }
+
+  unsubscribe(type: string): void {
+    // use args(type) for unsubscribe events by use filter to filtering array not in args(type) into subscribers
+    this.subscribers = this.subscribers.filter((data: IEvent) => data.type() != type)
+  }
+}
+
+class LowStockWarningEvent implements IEvent {
+  // same as another IEvent Interface for proper calling and implementation
+  constructor(private readonly _quantity: number, private readonly _machineId: string) {}
+
+  machineId(): string {
+    return this._machineId;
+  }
+
+  // checkStock() by if less than 3 will return true;
+  lowStock(): boolean {
+    return this._quantity < 3;
+  }
+
+  type(): string {
+    return 'check';
+  }
+}
+
+class StockLevelOkEvent implements IEvent {
+  // same as another IEvent Interface for proper calling and implementation
+  constructor(private readonly _quantity: number, private readonly _machineId: string) {}
+
+  machineId(): string {
+    return this._machineId;
+  }
+
+  // checkStock() by if less than 3 will return true;
+  checkStock(): boolean {
+    return this._quantity >= 3;
+  }
+
+  type(): string {
+    return 'check';
+  }
 }
 
 
@@ -24,7 +104,7 @@ class MachineSaleEvent implements IEvent {
   }
 
   getSoldQuantity(): number {
-    return this._sold
+    return this._sold;
   }
 
   type(): string {
@@ -36,11 +116,17 @@ class MachineRefillEvent implements IEvent {
   constructor(private readonly _refill: number, private readonly _machineId: string) {}
 
   machineId(): string {
-    throw new Error("Method not implemented.");
+    return this._machineId;
   }
 
+  // implements getRefillQuantity same as "getSoldQuantity()"
+  getRefillQuantity(): number {
+    return this._refill;
+  }
+
+  // add "refill" string same as "sold"
   type(): string {
-    throw new Error("Method not implemented.");
+    return "refill";
   }
 }
 
@@ -48,17 +134,58 @@ class MachineSaleSubscriber implements ISubscriber {
   public machines: Machine[];
 
   constructor (machines: Machine[]) {
-    this.machines = machines; 
+    this.machines = machines;
   }
 
+  // add console.log() for visualize process and data
   handle(event: MachineSaleEvent): void {
     this.machines[2].stockLevel -= event.getSoldQuantity();
+    console.log(`Sold at Machine ${event.machineId()} to ${this.machines[2].stockLevel} by ${event.getSoldQuantity()}`);
   }
 }
 
 class MachineRefillSubscriber implements ISubscriber {
-  handle(event: IEvent): void {
-    throw new Error("Method not implemented.");
+  public machines: Machine[];
+
+  constructor (machines: Machine[]) {
+    this.machines = machines;
+  }
+
+  // add console.log() for visualize process and data
+  handle(event: MachineRefillEvent): void {
+    this.machines[2].stockLevel += event.getRefillQuantity();
+    console.log(`Refill at Machine ${event.machineId()} from ${event.getRefillQuantity()} to ${this.machines[2].stockLevel}`);
+  }
+}
+
+// StockWarningSubscriber for Subscribe a Sales / Refill Event for Observe Stocks and Warning via console.log()
+class StockWarningSubscriber implements ISubscriber {
+  public machines: Machine[];
+
+  // check both stock,warning use object to store
+  protected fired: {
+    stock: boolean;
+    warning: boolean;
+  };
+
+  constructor (machines: Machine[]) {
+    this.machines = machines;
+    this.fired = {
+      stock: false,
+      warning: false,
+    };
+  }
+
+  // handle(event: StockLevelOkEvent or LowStockWarningEvent) for decision between incoming events for different warning and different one time firing notification
+  handle(event: StockLevelOkEvent | LowStockWarningEvent): void {
+    if (event instanceof LowStockWarningEvent && event.lowStock() && !this.fired.warning) {
+      console.log(`Now Stock of ${event.machineId()} is Low !`);
+      this.fired.warning = true;
+    }
+    if (event instanceof StockLevelOkEvent && event.checkStock() && !this.fired.stock){
+      console.log(`Now Stock of ${event.machineId()} is OK !`);
+      this.fired.stock = true;
+    }
   }
 }
 
@@ -86,14 +213,17 @@ const randomMachine = (): string => {
 
 }
 
-const eventGenerator = (): IEvent => {
+const eventGenerator = (): IEvent[] => {
   const random = Math.random();
+  let rd = randomMachine();
   if (random < 0.5) {
     const saleQty = Math.random() < 0.5 ? 1 : 2; // 1 or 2
-    return new MachineSaleEvent(saleQty, randomMachine());
+    // add StockLevelOkEvent and LowStockWarningEvent for Add Both of (StockLevelOk/LowStockWarning) Events for Observation
+    return [new MachineSaleEvent(saleQty, rd), new StockLevelOkEvent(saleQty, rd), new LowStockWarningEvent(saleQty, rd)];
   } 
   const refillQty = Math.random() < 0.5 ? 3 : 5; // 3 or 5
-  return new MachineRefillEvent(refillQty, randomMachine());
+  // add StockLevelOkEvent and LowStockWarningEvent for Add Both of (StockLevelOk/LowStockWarning) Events for Observation
+  return [new MachineRefillEvent(refillQty, rd), new StockLevelOkEvent(refillQty, rd), new LowStockWarningEvent(refillQty, rd)];
 }
 
 
@@ -104,13 +234,32 @@ const eventGenerator = (): IEvent => {
 
   // create a machine sale event subscriber. inject the machines (all subscribers should do this)
   const saleSubscriber = new MachineSaleSubscriber(machines);
+  // add MachineRefillSubscriber for Add Machine Refill
+  const refillSubscriber = new MachineRefillSubscriber(machines);
+  // add StockWarningSubscriber for Add Stock Warning
+  const stockCheckerSubscriber = new StockWarningSubscriber(machines);
 
   // create the PubSub service
-  const pubSubService: IPublishSubscribeService = null as unknown as IPublishSubscribeService; // implement and fix this
+  const pubSubService: PubSubService = new PubSubService(); // implement and fix this
 
   // create 5 random events
   const events = [1,2,3,4,5].map(i => eventGenerator());
 
   // publish the events
-  events.map(pubSubService.publish);
+  events.map((data) => {
+    pubSubService.publish(data);
+  });
+
+  // Subscribe Sales Event
+  pubSubService.subscribe('sale',saleSubscriber);
+  // Subscribe Check Event
+  pubSubService.subscribe('check', stockCheckerSubscriber);
+  // Unsubscribe Sales Event
+  pubSubService.unsubscribe('sale');
+  // Subscribe Refill Event
+  pubSubService.subscribe('refill',refillSubscriber);
+  // Unsubscribe Refill Event
+  pubSubService.unsubscribe('refill');
+  // Unsubscribe Check Event
+  pubSubService.unsubscribe('check');
 })();
